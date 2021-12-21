a7e9c5c537963c7061e1716e25ba38a5b5a6f411
Md. Arshad Ahmed <mdarshad_ug@cse.nits.ac.in>
2021-12-21 16:20:11 +0000

    added tasks  1-5

---
+++ hello_ockam/src/lib.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+// src/lib.rs
+
+mod echoer;
+pub use echoer::*;
+
+mod hop;
+pub use hop::*;
